46a60048f3ddf56e6682d1c7221848f9a534b3d7
rice cracker <dangkhoavtnt223@gmail.com>
2022-03-25 09:41:32 +0700

    fixed code style

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
